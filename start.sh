93387aa5809a2557a4448f1656e78b752f1dccad
Vihara Senindu <110663030+viharasenindu@users.noreply.github.com>
2022-09-24 08:37:55 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/viharasenindu22/movie.git /EvaMaria
+  git clone https://github.com/viharasenindu/movieseriesbot.git /EvaMaria
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /EvaMaria
